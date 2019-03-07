b95ae77f7eeaf701e6a8219b917233cdde4bf4d1
anarckk <anarckk@gmail.com>
2019-03-07 09:53:06 +0800

    webpack 修改自动刷新页面

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as _ from 'lodash';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 function component() {
     const element = document.createElement('div');
@@ -7,4 +9,18 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+interval(1000).pipe(take(3)).subscribe(i => {
+    const promise = new Promise(resolve => {
+        setTimeout(() => {
+            resolve('sb12345');
+        }, 100);
+    });
+    promise.then(data => {
+        console.log(i + '第一个链式调用', data);
+        return Promise.resolve({a: 'sb'});
+    }).then(data => {
+        console.log(i + '第二个链式调用', data);
+    });
+});
 
